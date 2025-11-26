150b47c9c2beb40c562718e0ea7b272cafa4874f
Anna Bispo <makiiras2@gmail.com>
2025-11-26 16:54:23 -0300

    Formulario foda

---
+++ primeiroprojetoti48/Form2.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace primeiroprojetoti48
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDlabel = new System.Windows.Forms.Label();
+            this.Nomelabel = new System.Windows.Forms.Label();
+            this.Telefonelabel = new System.Windows.Forms.Label();
+            this.Emaillabel = new System.Windows.Forms.Label();
+            this.Addbnt = new System.Windows.Forms.Button();
+            this.Altbnt = new System.Windows.Forms.Button();
+            this.Exbnt = new System.Windows.Forms.Button();
+            this.Consultbnt = new System.Windows.Forms.Button();
+            this.MDbnt = new System.Windows.Forms.Button();
+            this.IDtxt = new System.Windows.Forms.TextBox();
+            this.Nometxt = new System.Windows.Forms.TextBox();
+            this.DataRegistro = new System.Windows.Forms.DateTimePicker();
+            this.Datelabel = new System.Windows.Forms.Label();
+            this.Teltxt = new System.Windows.Forms.TextBox();
+            this.Emailtxt = new System.Windows.Forms.TextBox();
+            this.GridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IDlabel
+            // 
+            this.IDlabel.AutoSize = true;
+            this.IDlabel.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDlabel.Location = new System.Drawing.Point(20, 31);
+            this.IDlabel.Name = "IDlabel";
+            this.IDlabel.Size = new System.Drawing.Size(34, 20);
+            this.IDlabel.TabIndex = 0;
+            this.IDlabel.Text = "ID:";
+            // 
+            // Nomelabel
+            // 
+            this.Nomelabel.AutoSize = true;
+            this.Nomelabel.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nomelabel.Location = new System.Drawing.Point(20, 72);
+            this.Nomelabel.Name = "Nomelabel";
+            this.Nomelabel.Size = new System.Drawing.Size(61, 20);
+            this.Nomelabel.TabIndex = 1;
+            this.Nomelabel.Text = "Nome:";
+            // 
+            // Telefonelabel
+            // 
+            this.Telefonelabel.AutoSize = true;
+            this.Telefonelabel.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Telefonelabel.Location = new System.Drawing.Point(20, 113);
+            this.Telefonelabel.Name = "Telefonelabel";
+            this.Telefonelabel.Size = new System.Drawing.Size(81, 20);
+            this.Telefonelabel.TabIndex = 2;
+            this.Telefonelabel.Text = "Telefone:";
+            // 
+            // Emaillabel
+            // 
+            this.Emaillabel.AutoSize = true;
+            this.Emaillabel.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Emaillabel.Location = new System.Drawing.Point(20, 157);
+            this.Emaillabel.Name = "Emaillabel";
+            this.Emaillabel.Size = new System.Drawing.Size(65, 20);
+            this.Emaillabel.TabIndex = 3;
+            this.Emaillabel.Text = "E-mail:";
+            // 
+            // Addbnt
+            // 
+            this.Addbnt.BackColor = System.Drawing.Color.LavenderBlush;
+            this.Addbnt.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Addbnt.Location = new System.Drawing.Point(24, 461);
+            this.Addbnt.Name = "Addbnt";
+            this.Addbnt.Size = new System.Drawing.Size(88, 32);
+            this.Addbnt.TabIndex = 5;
+            this.Addbnt.Text = "Adicionar";
+            this.Addbnt.UseVisualStyleBackColor = false;
+            this.Addbnt.Click += new System.EventHandler(this.Addbnt_Click);
+            // 
+            // Altbnt
+            // 
+            this.Altbnt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Altbnt.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Altbnt.Location = new System.Drawing.Point(137, 461);
+            this.Altbnt.Name = "Altbnt";
+            this.Altbnt.Size = new System.Drawing.Size(88, 32);
+            this.Altbnt.TabIndex = 6;
+            this.Altbnt.Text = "Alterar";
+            this.Altbnt.UseVisualStyleBackColor = false;
+            this.Altbnt.Click += new System.EventHandler(this.Altbnt_Click);
+            // 
+            // Exbnt
+            // 
+            this.Exbnt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Exbnt.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exbnt.Location = new System.Drawing.Point(258, 461);
+            this.Exbnt.Name = "Exbnt";
+            this.Exbnt.Size = new System.Drawing.Size(88, 32);
+            this.Exbnt.TabIndex = 7;
+            this.Exbnt.Text = "Excluir";
+            this.Exbnt.UseVisualStyleBackColor = false;
+            this.Exbnt.Click += new System.EventHandler(this.Exbnt_Click);
+            // 
+            // Consultbnt
+            // 
+            this.Consultbnt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Consultbnt.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Consultbnt.Location = new System.Drawing.Point(373, 461);
+            this.Consultbnt.Name = "Consultbnt";
+            this.Consultbnt.Size = new System.Drawing.Size(88, 32);
+            this.Consultbnt.TabIndex = 8;
+            this.Consultbnt.Text = "Consultar";
+            this.Consultbnt.UseVisualStyleBackColor = false;
+            this.Consultbnt.Click += new System.EventHandler(this.Consultbnt_Click);
+            // 
+            // MDbnt
+            // 
+            this.MDbnt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.MDbnt.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MDbnt.Location = new System.Drawing.Point(491, 461);
+            this.MDbnt.Name = "MDbnt";
+            this.MDbnt.Size = new System.Drawing.Size(96, 32);
+            this.MDbnt.TabIndex = 9;
+            this.MDbnt.Text = "Mostrar Dados";
+            this.MDbnt.UseVisualStyleBackColor = false;
+            this.MDbnt.Click += new System.EventHandler(this.MDbnt_Click);
+            // 
+            // IDtxt
+            // 
+            this.IDtxt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.IDtxt.Location = new System.Drawing.Point(113, 31);
+            this.IDtxt.Name = "IDtxt";
+            this.IDtxt.Size = new System.Drawing.Size(257, 20);
+            this.IDtxt.TabIndex = 12;
+            // 
+            // Nometxt
+            // 
+            this.Nometxt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Nometxt.Location = new System.Drawing.Point(113, 72);
+            this.Nometxt.Name = "Nometxt";
+            this.Nometxt.Size = new System.Drawing.Size(474, 20);
+            this.Nometxt.TabIndex = 13;
+            // 
+            // DataRegistro
+            // 
+            this.DataRegistro.CalendarMonthBackground = System.Drawing.SystemColors.InactiveBorder;
+            this.DataRegistro.Location = new System.Drawing.Point(24, 425);
+            this.DataRegistro.Name = "DataRegistro";
+            this.DataRegistro.Size = new System.Drawing.Size(563, 20);
+            this.DataRegistro.TabIndex = 14;
+            // 
+            // Datelabel
+            // 
+            this.Datelabel.AutoSize = true;
+            this.Datelabel.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Datelabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Datelabel.Location = new System.Drawing.Point(21, 212);
+            this.Datelabel.Name = "Datelabel";
+            this.Datelabel.Size = new System.Drawing.Size(50, 16);
+            this.Datelabel.TabIndex = 16;
+            this.Datelabel.Text = "Registro";
+            // 
+            // Teltxt
+            // 
+            this.Teltxt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Teltxt.Location = new System.Drawing.Point(113, 113);
+            this.Teltxt.Name = "Teltxt";
+            this.Teltxt.Size = new System.Drawing.Size(474, 20);
+            this.Teltxt.TabIndex = 17;
+            // 
+            // Emailtxt
+            // 
+            this.Emailtxt.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Emailtxt.Location = new System.Drawing.Point(113, 157);
+            this.Emailtxt.Name = "Emailtxt";
+            this.Emailtxt.Size = new System.Drawing.Size(474, 20);
+            this.Emailtxt.TabIndex = 18;
+            // 
+            // GridView
+            // 
+            this.GridView.BackgroundColor = System.Drawing.Color.LavenderBlush;
+            this.GridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView.Location = new System.Drawing.Point(24, 231);
+            this.GridView.Name = "GridView";
+            this.GridView.Size = new System.Drawing.Size(563, 188);
+            this.GridView.TabIndex = 19;
+            this.GridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridView_CellContentClick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(610, 518);
+            this.Controls.Add(this.GridView);
+            this.Controls.Add(this.Emailtxt);
+            this.Controls.Add(this.Teltxt);
+            this.Controls.Add(this.Datelabel);
+            this.Controls.Add(this.DataRegistro);
+            this.Controls.Add(this.Nometxt);
+            this.Controls.Add(this.IDtxt);
+            this.Controls.Add(this.MDbnt);
+            this.Controls.Add(this.Consultbnt);
+            this.Controls.Add(this.Exbnt);
+            this.Controls.Add(this.Altbnt);
+            this.Controls.Add(this.Addbnt);
+            this.Controls.Add(this.Emaillabel);
+            this.Controls.Add(this.Telefonelabel);
+            this.Controls.Add(this.Nomelabel);
+            this.Controls.Add(this.IDlabel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label IDlabel;
+        private System.Windows.Forms.Label Nomelabel;
+        private System.Windows.Forms.Label Telefonelabel;
+        private System.Windows.Forms.Label Emaillabel;
+        private System.Windows.Forms.Button Addbnt;
+        private System.Windows.Forms.Button Altbnt;
+        private System.Windows.Forms.Button Exbnt;
+        private System.Windows.Forms.Button Consultbnt;
+        private System.Windows.Forms.Button MDbnt;
+        private System.Windows.Forms.TextBox IDtxt;
+        private System.Windows.Forms.TextBox Nometxt;
+        private System.Windows.Forms.DateTimePicker DataRegistro;
+        private System.Windows.Forms.Label Datelabel;
+        private System.Windows.Forms.TextBox Teltxt;
+        private System.Windows.Forms.TextBox Emailtxt;
+        private System.Windows.Forms.DataGridView GridView;
+    }
+}
---
+++ primeiroprojetoti48/Form2.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeiroprojetoti48
+{
+    public partial class Form2 : Form
+    {
+        List<Contato> lista = new List<Contato>();
+        int proximoId = 1;
+
+        public class Contato
+        {
+            public int ID { get; set; }
+            public string Nome { get; set; }
+            public string Telefone { get; set; }
+            public string Email { get; set; }
+            public DateTime Dt { get; set; }
+        }
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        bool EmailValido(string email)
+        {
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+
+        private void LimparCampos()
+        {
+            IDtxt.Clear();
+            Nometxt.Clear();
+            Teltxt.Clear();
+            Emailtxt.Clear();
+            DataRegistro.Value = DateTime.Now;
+
+            Nometxt.Focus();
+        }
+
+        private void AtualizarGrid()
+        {
+            GridView.DataSource = null;
+            GridView.DataSource = lista;
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Nometxt.Focus();
+        }
+
+
+        /*
+        =========================================
+                    BOTÃO ADICIONAR
+        =========================================*/
+        private void Addbnt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Nometxt.Text))
+            {
+                MessageBox.Show("Informe o nome!");
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(Emailtxt.Text) && !EmailValido(Emailtxt.Text))
+            {
+                MessageBox.Show("Informe um email válido");
+                return;
+
+            }
+
+
+            Contato c = new Contato();
+            {
+                c.ID = proximoId++;
+                c.Nome = Nometxt.Text;
+                c.Telefone = Teltxt.Text;
+                c.Email = Emailtxt.Text;
+                c.Dt = DataRegistro.Value;
+            }
+
+            lista.Add(c);
+            AtualizarGrid();
+            LimparCampos();
+        }
+
+
+        /*
+        =========================================
+                    BOTÃO ALTERAR
+        =========================================*/
+        private void Altbnt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(IDtxt.Text, out int id))
+            {
+                MessageBox.Show("Selecione um registro para alterar");
+                return;
+            }
+
+            int ID = int.Parse(IDtxt.Text);
+            var contato = lista.FirstOrDefault(c => c.ID == id);
+            if (contato == null)
+            {
+                MessageBox.Show("Registro não encontrado!");
+                return;
+            }
+
+            contato.Nome = Nometxt.Text;
+            contato.Email = Emailtxt.Text;
+            contato.Telefone = Teltxt.Text;
+            contato.Dt = DataRegistro.Value;
+
+            AtualizarGrid();
+            LimparCampos();
+            MessageBox.Show("Contato alterado com sucesso!");
+        }
+
+
+        /*
+        =========================================
+                    BOTÃO EXCLUIR
+        =========================================*/
+        private void Exbnt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(IDtxt.Text, out int id))
+            {
+                MessageBox.Show("Selecione um contato para excluir!");
+                return;
+            }
+
+            var contato = lista.FirstOrDefault(c => c.ID == id);
+            if (contato != null)
+            {
+                lista.Remove(contato);
+                AtualizarGrid();
+                LimparCampos();
+                MessageBox.Show("Contato excluído com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Contato não encontrado!");
+            }
+        }
+
+
+
+        /*
+        =========================================
+                    BOTÃO CONSULTAR
+        =========================================*/
+        private void Consultbnt_Click(object sender, EventArgs e)
+        {
+            string nomeBusca = Nometxt.Text.Trim().ToLower();
+            if (string.IsNullOrWhiteSpace(nomeBusca))
+            {
+                MessageBox.Show("Informe o nome para consultar!");
+                return;
+            }
+
+            var resultados = lista.Where(c => c.Nome.ToLower().Contains(nomeBusca)).ToList();
+
+            if (resultados.Count > 0)
+            {
+                GridView.DataSource = null;
+                GridView.DataSource = resultados;
+            }
+            else
+            {
+                MessageBox.Show("Nenhum contato encontrado!");
+            }
+        }
+
+
+        /*
+        =========================================
+                   BOTÃO MOSTRAR DADOS
+        =========================================*/
+        private void MDbnt_Click(object sender, EventArgs e)
+        {
+            AtualizarGrid();
+        }
+
+        private void GridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var contato = lista[e.RowIndex];
+
+                IDtxt.Text = contato.ID.ToString();
+                Nometxt.Text = contato.Nome;
+                Teltxt.Text = contato.Telefone;
+                Emailtxt.Text = contato.Email;
+                DataRegistro.Value = contato.Dt;
+
+            }
+        }
+
+
+    }
+}
